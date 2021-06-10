2c3792d62bc3237d5f98feb454e61cd85323737f
Koy <369491420@qq.com>
2021-06-10 22:36:22 +0800

    add: history command.

---
+++ src/main/java/com/koy/kaviewer/command/KaViewerCommand.java
@@ -0,0 +1,24 @@
+package com.koy.kaviewer.command;
+
+import com.koy.kaviewer.common.DefaultValues;
+import com.koy.kaviewer.core.KaViewerCommandHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+@ShellCommandGroup("Built-In Commands")
+public class KaViewerCommand {
+
+    @Autowired
+    private KaViewerCommandHistory kaViewerCommandHistory;
+
+    @ShellMethod(value = "show history command", key = {"history", "his"})
+    public void history(@ShellOption(defaultValue = DefaultValues.DEFAULT_NONE) String token) {
+        kaViewerCommandHistory.showHis(token);
+    }
+
+
+}
