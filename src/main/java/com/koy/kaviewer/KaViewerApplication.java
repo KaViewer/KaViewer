02ba220f21de526ab79b2485ccc0e427f7e70ccf
Koy <369491420@qq.com>
2021-06-20 23:34:31 +0800

    fix: fix configuration.

--- src/main/java/com/koy/kaviewer/KaViewerApplication.java
+++ src/main/java/com/koy/kaviewer/KaViewerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.shell.jline.JLineShellAutoConfiguration;
 
-@SpringBootApplication(exclude = {JLineShellAutoConfiguration.class})
+@SpringBootApplication
 public class KaViewerApplication {
 
     public static void main(String[] args) {
