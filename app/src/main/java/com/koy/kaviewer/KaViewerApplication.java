29bf7b70b4a51c893c723a0feadf01c69fd49da2
Koy <369491420@qq.com>
2022-04-30 23:27:13 +0800

    update: change modules.

--- app/src/main/java/com/koy/kaviewer/KaViewerApplication.java
+++ app/src/main/java/com/koy/kaviewer/KaViewerApplication.java
@@ -1,11 +1,12 @@
 package com.koy.kaviewer;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(scanBasePackages = {"com.koy.kaviewer"})
 public class KaViewerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KaViewerApplication.class, args);
     }
---
+++ rest/src/main/java/com/koy/kaviewer/api/TopicController.java
@@ -0,0 +1,14 @@
+package com.koy.kaviewer.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/topic")
+public class TopicController {
+
+    public ResponseEntity<String> list() {
+        return null;
+    }
+}
