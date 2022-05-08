5f28e461905d0c177d70c7a84c886c24ecdb7eb8
Koy <369491420@qq.com>
2022-05-08 18:57:06 +0800

    chore: code clean.

--- app/src/main/java/com/koy/kaviewer/service/KafkaSetUpService.java
+++ app/src/main/java/com/koy/kaviewer/service/KafkaSetUpService.java
@@ -2,15 +2,11 @@ package com.koy.kaviewer.service;
 
 import com.koy.kaviewer.KaViewerApplication;
 import com.koy.kaviewer.kafka.application.KafkaApplication;
-import com.koy.kaviewer.kafka.core.ConfigResolver;
-import com.koy.kaviewer.kafka.core.KafkaProperties;
 import com.koy.kaviewer.kafka.core.KafkaSetUpHandler;
 import com.koy.kaviewer.kafka.core.PropertiesResources;
-import com.koy.kaviewer.kafka.entity.KafkaApplicationCacheEntity;
 import com.koy.kaviewer.kafka.service.KafkaService;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
@@ -32,11 +28,6 @@ public class KafkaSetUpService implements KafkaSetUpHandler {
         final ConfigurableApplicationContext kafka = applicationBuilder.run(args);
         final KafkaService kafkaService = kafka.getBean(KafkaService.class);
         kafkaService.buildApplication(resources);
-//        final ConfigurableApplicationContext client = applicationBuilder.child(ClientApplication.class)
-//                .web(WebApplicationType.NONE)
-//                .parent(kafka)
-//                .properties("spring.application.name=KaViewer-Kafka-Client")
-//                .run(args);
     }
 
 
