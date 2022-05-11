4c75137214193531858aa02a433a3b5974a13416
Koy <369491420@qq.com>
2022-05-11 22:43:21 +0800

    chore: code clean.

--- kafka/src/main/java/com/koy/kaviewer/kafka/application/ClientApplication.java
+++
@@ -1,6 +0,0 @@
-package com.koy.kaviewer.kafka.application;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = {"com.koy.kaviewer.kafka.client"})
-public class ClientApplication {
-}
--- kafka/src/main/java/com/koy/kaviewer/kafka/application/ProducerApplication.java
+++
@@ -1,7 +0,0 @@
-package com.koy.kaviewer.kafka.application;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"com.koy.kaviewer.kafka.producer"})
-public class ProducerApplication {
-}
