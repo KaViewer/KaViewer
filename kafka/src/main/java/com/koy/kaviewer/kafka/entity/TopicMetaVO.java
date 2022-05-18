c18f4465f41f1f9518b62f3d3af226b922043fbb
Koy <369491420@qq.com>
2022-05-18 23:53:51 +0800

    chore: code clean.

--- kafka/src/main/java/com/koy/kaviewer/kafka/entity/TopicMetaVO.java
+++ kafka/src/main/java/com/koy/kaviewer/kafka/entity/TopicMetaVO.java
@@ -1,6 +1,5 @@
 package com.koy.kaviewer.kafka.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
