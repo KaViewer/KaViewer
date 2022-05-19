ab0eff79db7c16599ef85d3b4df9fc10adf1c305
Koy <369491420@qq.com>
2022-05-19 21:59:32 +0800

    api: add consumer.

---
+++ rest/src/main/java/com/koy/kaviewer/rest/api/ConsumerController.java
@@ -0,0 +1,32 @@
+package com.koy.kaviewer.rest.api;
+
+import com.koy.kaviewer.rest.domain.MessageRecord;
+import com.koy.kaviewer.rest.service.ConsumerBizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/topic")
+public class ConsumerController {
+
+    @Autowired
+    ConsumerBizService consumerBizService;
+
+    @GetMapping("/{topic}/p/{partition}/s/{sorted}/{limit}")
+    public ResponseEntity<List<MessageRecord<String, String>>> fetch(
+            @PathVariable(name = "topic") String topic,
+            @PathVariable(name = "partition") Integer partition,
+            @PathVariable(name = "sorted") String sorted,
+            @PathVariable(name = "limit") Integer limit) {
+        final List<MessageRecord<String, String>> records = consumerBizService.fetch(topic, partition, limit, sorted);
+        return ResponseEntity.ok(records);
+
+    }
+
+}
