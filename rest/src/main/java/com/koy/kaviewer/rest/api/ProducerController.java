d3fed8003ae3dd1c2050f1446e519646dafabf8c
Koy <369491420@qq.com>
2022-06-03 23:34:36 +0800

    update: refine producer api.

--- rest/src/main/java/com/koy/kaviewer/rest/api/ProducerController.java
+++ rest/src/main/java/com/koy/kaviewer/rest/api/ProducerController.java
@@ -4,7 +4,9 @@ import com.koy.kaviewer.rest.domain.HeaderVO;
 import com.koy.kaviewer.rest.domain.MessageHeaders;
 import com.koy.kaviewer.rest.domain.MessageVO;
 import com.koy.kaviewer.rest.service.ProducerBizService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,8 +26,27 @@ public class ProducerController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> publish(@RequestBody MessageVO messageVO) {
+        if (messageVO.inValid()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         producerBizService.publish(messageVO);
-        return ResponseEntity.ok(null);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> publish(
+            @RequestParam(name = "key") String key,
+            @RequestParam(name = "value") MultipartFile val,
+            @RequestParam(name = "topic") String topic,
+            @RequestParam(name = "partition") int partition,
+            @MessageHeaders(name = "headers") List<HeaderVO> headers
+
+    ) {
+        if (StringUtils.isEmpty(key) || val.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        producerBizService.publish(key, val, topic, partition, headers);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -37,7 +58,10 @@ public class ProducerController {
             @MessageHeaders(name = "headers") List<HeaderVO> headers
 
     ) {
+        if (key.isEmpty() || val.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         producerBizService.publish(key, val, topic, partition, headers);
-        return ResponseEntity.ok(null);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
