1a48b1c6d96a3689716eb6bbdcafa57431c5d13a
Koy <369491420@qq.com>
2022-06-11 00:39:44 +0800

    add: EnvResolver.

---
+++ app/src/main/java/com/koy/kaviewer/app/service/resolver/EnvResolver.java
@@ -0,0 +1,25 @@
+package com.koy.kaviewer.app.service.resolver;
+
+import com.koy.kaviewer.kafka.core.PropertiesResources;
+import org.springframework.core.env.Environment;
+
+import java.util.StringJoiner;
+
+public interface EnvResolver<R> {
+    String KAVIEWER_PREFIX = "kaviewer.";
+
+    static String kaViewerConfigKey(String... properties) {
+        final StringJoiner stringJoiner = new StringJoiner(".", KAVIEWER_PREFIX, "");
+        for (String property : properties) {
+            stringJoiner.add(property);
+        }
+
+        return stringJoiner.toString();
+
+    }
+
+
+    PropertiesResources<R> load(Environment environment);
+
+    boolean support(PropertiesResources.ResourcesType resourcesType);
+}
