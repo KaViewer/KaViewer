f6315616b1087817797460fd3fc8239893375844
Koy <369491420@qq.com>
2022-06-13 03:17:35 +0800

    chore: code clean.

--- app/src/main/java/com/koy/kaviewer/app/service/KaViewerEnvScanner.java
+++ app/src/main/java/com/koy/kaviewer/app/service/KaViewerEnvScanner.java
@@ -15,12 +15,16 @@ import java.util.Objects;
 @Service
 public class KaViewerEnvScanner {
 
+    private final List<EnvResolver> envResolvers;
+    private final KafkaApplicationSetupService kafkaApplicationSetupService;
+    private final Environment environment;
+
     @Autowired
-    List<EnvResolver> envResolvers;
-    @Autowired
-    KafkaApplicationSetupService kafkaApplicationSetupService;
-    @Autowired
-    Environment environment;
+    public KaViewerEnvScanner(List<EnvResolver> envResolvers, KafkaApplicationSetupService kafkaApplicationSetupService, Environment environment) {
+        this.envResolvers = envResolvers;
+        this.kafkaApplicationSetupService = kafkaApplicationSetupService;
+        this.environment = environment;
+    }
 
     @PostConstruct
     public void scanEnv() {
