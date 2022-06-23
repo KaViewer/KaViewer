ce5df10dcb83d9e72346495aac22beda27bf562b
Koy <369491420@qq.com>
2022-06-23 23:13:29 +0800

    update: actuator info.

---
+++ app/src/main/java/com/koy/kaviewer/app/service/KaViewerIntrospectionContributor.java
@@ -0,0 +1,20 @@
+package com.koy.kaviewer.app.service;
+
+import com.koy.kaviewer.kafka.application.KafkaApplication;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class KaViewerIntrospectionContributor implements InfoContributor {
+
+    @Override
+    // TODO add more info
+    public void contribute(Info.Builder builder) {
+        final List<String> kafkaContexts = KafkaApplication.listAll();
+        builder.withDetail("kafkaContexts", kafkaContexts);
+    }
+
+}
