cf4238203bf8a5a860824bf7cc6beeb5dde0a163
Koy <369491420@qq.com>
2022-06-25 23:28:32 +0800

    add: contexts endpoint.

---
+++ app/src/main/java/com/koy/kaviewer/app/service/endpoint/KaViewerBeansEndpoint.java
@@ -0,0 +1,40 @@
+package com.koy.kaviewer.app.service.endpoint;
+
+import com.koy.kaviewer.kafka.application.KafkaApplication;
+import com.koy.kaviewer.rest.KaViewerRestApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.beans.BeansEndpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Component
+@WebEndpoint(id = "contexts")
+public class KaViewerBeansEndpoint {
+    private final ConfigurableApplicationContext kaViewerApplicationContext;
+
+    @Autowired
+    public KaViewerBeansEndpoint(ConfigurableApplicationContext kaViewerApplicationContext) {
+        this.kaViewerApplicationContext = kaViewerApplicationContext;
+    }
+
+
+    @ReadOperation
+    public BeansEndpoint.ApplicationBeans beans() {
+        final ConfigurableApplicationContext rest = (ConfigurableApplicationContext) KaViewerRestApplication.getRest();
+        final BeansEndpoint restBeanEndPoint = new BeansEndpoint(rest);
+        final BeansEndpoint.ApplicationBeans restBeans = restBeanEndPoint.beans();
+        final ConcurrentLinkedQueue<ApplicationContext> kafkaApplicationContexts = KafkaApplication.getKafkaApplicationContexts();
+        kafkaApplicationContexts.forEach(kafkaApplicationContext -> {
+            final BeansEndpoint kafkaBeanEndPoint = new BeansEndpoint((ConfigurableApplicationContext) kafkaApplicationContext);
+            final BeansEndpoint.ApplicationBeans kafkaBeans = kafkaBeanEndPoint.beans();
+            restBeans.getContexts().put(kafkaApplicationContext.getId(), kafkaBeans.getContexts().get(kafkaApplicationContext.getId()));
+        });
+        return restBeans;
+
+    }
+}
