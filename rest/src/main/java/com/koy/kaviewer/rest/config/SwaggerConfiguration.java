79c8f5dc6298cb11c48f2fc1adc8e692294b5231
Koy <369491420@qq.com>
2022-06-26 16:57:44 +0800

    update: swagger config.

--- rest/src/main/java/com/koy/kaviewer/rest/config/SwaggerConfiguration.java
+++ rest/src/main/java/com/koy/kaviewer/rest/config/SwaggerConfiguration.java
@@ -1,20 +1,39 @@
 package com.koy.kaviewer.rest.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
+    @Value("v${kaviewer.version:x,y.z}")
+    private String kaViewerVersion;
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.koy.kaviewer.rest.api"))
                 .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("KaViewer")
+                .license("Apache License Version 2.0")
+                .description("KaViewer Swagger API docs")
+                .contact(new Contact("@Koy", "https://github.com/Koooooo-7", StringUtils.EMPTY))
+                .version(kaViewerVersion)
                 .build();
     }
 }
