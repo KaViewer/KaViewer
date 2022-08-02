4934f88a4f3d5fef5c2b260b49daad8f3a0feebb
Koy <369491420@qq.com>
2022-08-02 17:04:25 +0800

    update: meta api doc.

--- web/src/main/java/com/koy/kaviewer/web/api/MetaController.java
+++ web/src/main/java/com/koy/kaviewer/web/api/MetaController.java
@@ -2,12 +2,15 @@ package com.koy.kaviewer.web.api;
 
 import com.koy.kaviewer.kafka.entity.KafkaPropertiesVO;
 import com.koy.kaviewer.web.service.MetaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Metadata Operations", description = "Get KaViewer Metadata.")
 @RestController
 @RequestMapping("/api/v1/meta")
 @CrossOrigin(origins = "*")
@@ -18,6 +21,7 @@ public class MetaController {
         this.metaService = metaService;
     }
 
+    @Operation(summary = "Get specific cluster's kafka properties meta.")
     @GetMapping
     public ResponseEntity<KafkaPropertiesVO> meta(){
         return ResponseEntity.ok(metaService.meta());
