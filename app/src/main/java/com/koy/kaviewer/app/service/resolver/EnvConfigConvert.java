c2a9862f62d9590ba479edb5699841e665d06c60
Koy <369491420@qq.com>
2022-08-22 16:46:51 +0800

    chore: missing.

---
+++ app/src/main/java/com/koy/kaviewer/app/service/resolver/EnvConfigConvert.java
@@ -0,0 +1,6 @@
+package com.koy.kaviewer.app.service.resolver;
+
+import org.springframework.core.convert.converter.Converter;
+
+public interface EnvConfigConvert<S, T> extends Converter<S, T> {
+}
