e31e05354c149863d9185a426088bbe32310bfde
Koy <369491420@qq.com>
2022-08-27 23:19:43 +0800

    add: permission api.

---
+++ web/src/main/java/com/koy/kaviewer/web/api/PermissionController.java
@@ -0,0 +1,12 @@
+package com.koy.kaviewer.web.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/permission")
+public class PermissionController {
+    @GetMapping
+    public void permission() {
+
+    }
+}
