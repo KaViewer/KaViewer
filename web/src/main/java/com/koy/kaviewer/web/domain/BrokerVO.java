15f5dfbbdd2fdb7bcabca2d06aff758d36d346e1
Koy <369491420@qq.com>
2022-09-01 23:24:19 +0800

    chore: polish.

--- web/src/main/java/com/koy/kaviewer/web/domain/BrokerVO.java
+++ web/src/main/java/com/koy/kaviewer/web/domain/BrokerVO.java
@@ -1,32 +1,17 @@
 package com.koy.kaviewer.web.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Data
+@ToString
 public class BrokerVO {
     private final Integer id;
     private final String host;
     private final Integer port;
     private final String rack;
-
-    public BrokerVO(Integer id, String host, Integer port, String rack) {
-        this.id = id;
-        this.host = host;
-        this.port = port;
-        this.rack = rack;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public Integer getPort() {
-        return port;
-    }
-
-    public String getRack() {
-        return rack;
-    }
 }
 
--- web/src/main/java/com/koy/kaviewer/web/domain/ClusterVO.java
+++ web/src/main/java/com/koy/kaviewer/web/domain/ClusterVO.java
@@ -1,36 +1,19 @@
 package com.koy.kaviewer.web.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ClusterVO {
     private String cluster;
     private List<BrokerVO> brokers;
     private Long createdTime;
-
-    public ClusterVO(String cluster, List<BrokerVO> brokers, Long createdTime) {
-        this.cluster = cluster;
-        this.brokers = brokers;
-        this.createdTime = createdTime;
-    }
-
-    public String getCluster() {
-        return cluster;
-    }
-
-    public void setCluster(String cluster) {
-        this.cluster = cluster;
-    }
-
-    public List<BrokerVO> getBrokers() {
-        return brokers;
-    }
-
-    public void setBrokers(List<BrokerVO> brokers) {
-        this.brokers = brokers;
-    }
-
-    public Long getCreatedTime() {
-        return createdTime;
-    }
 }
 
