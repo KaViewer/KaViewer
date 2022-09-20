d439d6c10c27bfadac251bdca3e8fd602e01c675
Koy <369491420@qq.com>
2022-09-20 10:41:41 +0800

    chore: config lite mode.

--- app/src/main/java/com/koy/kaviewer/app/core/KaViewerPropertySourceLoaderConfig.java
+++ app/src/main/java/com/koy/kaviewer/app/core/KaViewerPropertySourceLoaderConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class KaViewerPropertySourceLoaderConfig {
 
     @Bean
